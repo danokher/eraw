170a5372969571627631b7b597dede7e22376769
Daniel Thomas O'Kane Hernández <93446070+danokher@users.noreply.github.com>
2023-11-18 05:12:52 +0100

    Update index.js

--- eraw-server/index.js
+++ eraw-server/index.js
@@ -5,7 +5,21 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 // middleware will be the connection to the frontend
-app.use(cors());
+// Middleware para la conexión al frontend
+app.use(cors({
+  origin: 'https://eraw-front2.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+// Middleware adicional para agregar encabezados CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://eraw-front2.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
